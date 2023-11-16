0ec0518833dd7b1be888f3fbae006adf6f8ed2e1
matthew123455647 <chanmatthewkl@gmail.com>
2023-11-16 22:53:57 +0800

    hi

--- tests/LoginTests.test.js
+++ tests/LoginTests.test.js
@@ -19,7 +19,7 @@ describe('Testing Login Function', () => {
     it('Should login successfully', async () => {
         const req = {
             body: {
-                email: 'john@gmail.com',
+                email: 'simon@gmail.com',
                 password: '123456',
             },
         };
